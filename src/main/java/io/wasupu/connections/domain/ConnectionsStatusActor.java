1c0ea0477b0c047256b834af7ea931ff42e02d54
raimundoangel.alegria <raimundoangel.alegria@bbva.com>
2018-04-01 00:12:53 +0200

    Has de configuration of all nodes shared

--- src/main/java/io/wasupu/connections/domain/ConnectionsStatusActor.java
+++ src/main/java/io/wasupu/connections/domain/ConnectionsStatusActor.java
@@ -5,9 +5,13 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.cluster.Cluster;
 import akka.cluster.ddata.*;
-import akka.cluster.ddata.Replicator.*;
+import akka.cluster.ddata.Replicator.Changed;
+import akka.cluster.ddata.Replicator.Subscribe;
+import akka.cluster.ddata.Replicator.Update;
+import akka.cluster.ddata.Replicator.WriteAll;
 import scala.concurrent.duration.Duration;
 
+import java.util.Set;
 import java.util.UUID;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -40,7 +44,7 @@ public class ConnectionsStatusActor extends AbstractActor {
                 })
                 .match(GetStatus.class, getStatus -> getSender().tell(new ConnectionsStatus(id, numberOfConnections), getSelf()))
                 .match(Changed.class, changed -> changed.key().equals(dataKey),
-                        changed -> receiveChanged((Replicator.Changed<ORSet<String>>) changed))
+                        changed -> receiveChanged((Replicator.Changed<ORMultiMap<String,Integer>>) changed))
                 .build();
     }
 
@@ -50,11 +54,11 @@ public class ConnectionsStatusActor extends AbstractActor {
         replicator.tell(subscribe, ActorRef.noSender());
     }
 
-    private void receiveChanged(Replicator.Changed<ORSet<String>> c) {
+    private void receiveChanged(Replicator.Changed<ORMultiMap<String,Integer>> c) {
         var data = c.dataValue();
         System.out.println(node.state());
         System.out.println("***************************************************************************");
-        System.out.println("Current elements: {}" + data.getElements());
+        System.out.println("Current elements: {}" + data);
         System.out.println("***************************************************************************");
     }
 
@@ -63,9 +67,9 @@ public class ConnectionsStatusActor extends AbstractActor {
 
         var update = new Update<>(
                 dataKey,
-                ORSet.create(),
+                ORMultiMap.create(),
                 writeAll,
-                curr -> curr.add(node, numberOfConnections.toString()));
+                curr -> curr.put(node, id, Set.of(numberOfConnections)));
 
         replicator.tell(update, getSelf());
     }
@@ -79,7 +83,5 @@ public class ConnectionsStatusActor extends AbstractActor {
 
     private final Cluster node = Cluster.get(getContext().getSystem());
 
-    private final Key<ORSet<String>> dataKey = ORSetKey.create("numberOfConnections");
-
-
+    private final Key<ORMultiMap<String, Integer>> dataKey = ORMultiMapKey.create("numberOfConnections");
 }
